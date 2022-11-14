bea9b3d8bf2946bd997a59f176a072d149ed8a1d
Peter Manda <109303643+PeterManda19@users.noreply.github.com>
2022-11-14 10:34:24 +0000

    Roll dice restructured to functions

--- main.py
+++ main.py
@@ -7,39 +7,38 @@ print("""Roll a dice with any number of sides (essentially as big of a number as
 print()
 print("Everytime you key in your response press enter.")
 
-def rollDice():
-  exit = ""
-  while exit.lower() != "no" or exit.lower() != "no":
+def promptUser():
+  while True:
     print()
     try:
       num = int(input("How many sides?: "))
       print()
+      return num
     except ValueError:
       print()
       print("I am expecting positive numbers.")
       print()
-      continue
-  
+      continue  
     if num <= 0:
       print("I don't like zero and negative numbers.")
       print("I am expecting positive integer numbers.")
       continue
-    else:
-      print("You rolled",random.randint(1,num))
 
-     
-    while exit.lower() != "yes":  
-      exit = input("Roll again? ") 
-      if exit.lower() == "no" or exit.lower() == "n":
-        print()
-        print("Thanks for playing Infinity Dice! 🎲")
-        break
-      elif exit.lower() == "yes" or exit.lower() == "y":
-        break
-      else:
-        print()
-        print("Am expecting 'yes' or 'no'; or 'y' or 'n' for yes and no respectively.")
-        continue
+
+def rollDice(num):
+    print("You rolled",random.randint(1,num))
+
+def quit():
+  exit = ""
+  while exit.lower() != "no" or exit.lower() != "n":  
+    exit = input("Roll again? ") 
+    if exit.lower() == "yes" or exit.lower() == "y" or exit =="":
+      num = promptUser()
+      rollDice(num) 
+    else:
+     print()
+     print("Shutting down Infinity Dice 🎲...")
+     break
       
 
 def endGame():
@@ -52,5 +51,7 @@ To play again please click Stop on top right page and click Run to try again wit
 
 
 if __name__=="__main__":
-  rollDice()
+  num = promptUser()
+  rollDice(num)
+  quit()
   endGame()
